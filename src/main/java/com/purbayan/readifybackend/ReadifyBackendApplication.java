e9e560ac717609882e701f46df62fb83670aa2db
purbayan2014 <purbayan2014@gmail.com>
2023-04-30 21:37:34 +0530

    init commit

---
+++ src/main/java/com/purbayan/readifybackend/ReadifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.purbayan.readifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadifyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadifyBackendApplication.class, args);
+    }
+
+}
